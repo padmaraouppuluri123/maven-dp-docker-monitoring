70b288f36cb7fc2fee5bae84e45c77091265910e
sushma-sm <gurajasushmamounika@gmail.com>
2025-03-14 18:46:39 +0530

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
+# Install Docker
 apt-get update
 apt-get install -y docker.io
 systemctl start docker
 systemctl enable docker
 
+# Download prometheus.yml from GitHub
+curl -o /etc/prometheus/prometheus.yml https://raw.githubusercontent.com/<your-username>/<your-repository>/main/prometheus.yml
+
 # Pull Docker images
 docker pull prom/prometheus
 docker pull grafana/grafana
 
-# Run Prometheus
+# Run Prometheus with the downloaded configuration file
 docker run -d --name prometheus \
   -p 9090:9090 \
-  -v /prometheus.yml:/etc/prometheus/prometheus.yml \
+  -v /etc/prometheus/prometheus.yml:/etc/prometheus/prometheus.yml \
   prom/prometheus
 
 # Run Grafana
