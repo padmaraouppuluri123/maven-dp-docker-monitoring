45852bd167a54db1903e58617622d497ebab576a
sushma-sm <gurajasushmamounika@gmail.com>
2025-03-17 15:52:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ provider "google" {
   region  = var.region
   zone    = var.zone
 }
+
 # Create a VM instance for monitoring
 resource "google_compute_instance" "monitoring_vm" {
   name         = "monitoring-vm"
@@ -18,16 +19,23 @@ resource "google_compute_instance" "monitoring_vm" {
   network_interface {
     network = "default"
     access_config {
-      // Allocate a one-to-one NAT IP to the instance
+      # Allocate a one-to-one NAT IP to the instance
     }
   }
 
   metadata_startup_script = <<-EOF
     #!/bin/bash
+    apt-get update
+    apt-get install -y google-cloud-sdk
     gsutil cp gs://my-terraform-bucket-535168da5ef156/init.sh /tmp/init.sh
     chmod +x /tmp/init.sh
     /tmp/init.sh
   EOF
+
+  service_account {
+    email  = "your-service-account@${var.project_id}.iam.gserviceaccount.com"
+    scopes = ["cloud-platform"]
+  }
 
   tags = ["prometheus-grafana"]
 }
@@ -42,5 +50,12 @@ resource "google_compute_firewall" "allow_prometheus_grafana" {
     ports    = ["9090", "3000"]
   }
 
-  target_tags = ["prometheus-grafana"]
+  source_ranges = ["0.0.0.0/0"]
+  target_tags   = ["prometheus-grafana"]
+}
+
+# Output the external IP of the monitoring VM
+output "monitoring_vm_ip" {
+  description = "The external IP address of the monitoring VM"
+  value       = google_compute_instance.monitoring_vm.network_interface[0].access_config[0].nat_ip
 }
